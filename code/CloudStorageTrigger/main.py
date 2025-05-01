38dfc0422ab992cc6cb0715e6c7ef7a698c948c4
janani-ravi-loony <janani@loonycorn.com>
2025-05-01 16:24:06 +0530

    Updated code

--- code/CloudStorageTrigger/main.py
+++ code/CloudStorageTrigger/main.py
@@ -1,20 +1,40 @@
-from wand.image import Image
+import functions_framework
 from google.cloud import storage
+from PIL import Image, ImageOps
+import io
 
 storage_client = storage.Client()
-dest_bucket = 'loony-oreilly-olt-bucket-dest'
+dest_bucket_name = "loony-oreilly-olt-bucket-dest"
 
-def transform_image(data, context):
+@functions_framework.cloud_event
+def transform_image(cloud_event):
+    data = cloud_event.data
 
-    bucket = storage_client.get_bucket(data['bucket'])
-    blob = bucket.get_blob(data['name'])
+    bucket_name = data["bucket"]
+    file_name = data["name"]
 
-    image_source_file = blob.download_as_string()
+    print(f"Bucket: {bucket_name}")
+    print(f"File: {file_name}")
 
-    des_bucket = storage_client.get_bucket(dest_bucket)
-    image_dest_blob = des_bucket.blob('negated_' + data['name'])
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    file_contents = blob.download_as_bytes()
+    print(f"Downloaded file: {file_name}")
 
-    with Image(blob=image_source_file) as image:
-        with image.clone() as clone:
-            clone.negate()
-            image_dest_blob.upload_from_string(clone.make_blob())
+    # Open the image using Pillow
+    image = Image.open(io.BytesIO(file_contents)).convert("RGB")
+
+    # Apply negation
+    negated_image = ImageOps.invert(image)
+
+    # Save to a buffer
+    buffer = io.BytesIO()
+    negated_image.save(buffer, format="PNG")
+    buffer.seek(0)
+
+    # Upload to destination bucket
+    dest_bucket = storage_client.bucket(dest_bucket_name)
+    new_file_name = "negated_" + file_name
+    dest_blob = dest_bucket.blob(new_file_name)
+    dest_blob.upload_from_file(buffer, content_type="image/png")
+    print(f"Uploaded negated image as '{new_file_name}' to destination bucket: {dest_bucket_name}")
--- code/startup_script.sh
+++ code/startup_script.sh
@@ -1,5 +1,7 @@
-apt-get update
-apt-get install nginx -y
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install nginx -y
 cat <<EOF >/var/www/html/index.nginx-debian.html
 <html><body><h1>WELCOME</h1>
 <p>And this is how you use startup scripts on the GCP!</p>
